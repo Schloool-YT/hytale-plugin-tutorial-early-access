862098fec8c5c13388453d0dfc17db9cbfec041a
Max Schlosser <mschlos3@hs-mittweida.de>
2026-01-17 18:21:08 +0100

    Add episode 04

--- src/main/java/de/schloool/tutorialplugin/TutorialPlugin.java
+++ src/main/java/de/schloool/tutorialplugin/TutorialPlugin.java
@@ -8,4 +8,23 @@ public class TutorialPlugin extends JavaPlugin {
     public TutorialPlugin(@NonNullDecl JavaPluginInit init) {
         super(init);
     }
+
+    @Override
+    protected void setup() {
+        super.setup();
+        getLogger().atInfo().log("Tutorial-Plugin durchläuft setup...");
+    }
+
+    @Override
+    protected void start() {
+        super.start();
+        getLogger().atInfo().log("Tutorial-Plugin durchläuft start...");
+        getLogger().atWarning().log("Das Plugin befindet sich noch in Entwicklung!");
+    }
+
+    @Override
+    protected void shutdown() {
+        super.shutdown();
+        getLogger().atInfo().log("Tutorial-Plugin wurde beendet.");
+    }
 }
