7e2d8ba2011186ba52150b5b0f0cb35addc714d1
Max Schlosser <mschlos3@hs-mittweida.de>
2026-01-17 15:30:05 +0100

    Initial commit

---
+++ src/main/java/de/schloool/tutorialplugin/TutorialPlugin.java
@@ -0,0 +1,11 @@
+package de.schloool.tutorialplugin;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+public class TutorialPlugin extends JavaPlugin {
+    public TutorialPlugin(@NonNullDecl JavaPluginInit init) {
+        super(init);
+    }
+}
